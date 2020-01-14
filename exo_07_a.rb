a007fd6d41d23c1ce370f82dab64e0baffd2feba
Matthieu Degré <matthieu.degre@gmail.com>
2020-01-14 11:49:59 +0100

    Ajout de exo 6, 7a, 7b, 7c et 8

---
+++ exo_07_a.rb
@@ -0,0 +1,3 @@
+puts "Bonjour, c'est quooi ton blase?"
+user_name = gets.chomp
+puts user_name # gets.chomp demande à l'utilisateur de rentrer du texte
---
+++ exo_07_c.rb
@@ -0,0 +1,6 @@
+user_name = gets.chomp
+puts user_name # Le progamme a affiche une string puis nous demande de saisir une entrée grace au clavier, et ensuite affiche notre entrée
+# Le programme b affiche une string puis utilise la fonction print pour insérer un > puis sur la même ligne nous demande de saisir une entrée grace au clavier puis l'affiche.
+# Le programme c nous demande de saisir une entrée grâce au clavier et l'affiche directement.
+
+# Entre a et b, il y a un caractère > qui diffère, et entre c et les 2 premiers, c'est qu'on n'affiche pas la question
