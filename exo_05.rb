7f45ec019bb19397fb32ef71f78d0aa860a48229
whedia <hedia.wejdi94@gmail.com>
2020-01-14 11:18:57 +0100

    Exercice 1 à 5 ajouté

---
+++ exo_05.rb
@@ -0,0 +1,22 @@
+puts "On va compter le nombre d'heure de travail à THP" #Affiche string
+puts "Travail : #{10 * 5 * 11}" #Affiche string et calcul de l'intérieur de l'expression entouré par dièse 
+puts "En minute ca fait : #{10 * 5 * 11 * 60}" #Affiche string et calcul de l'intérieur de l'expression entouré par dièse 
+
+puts "Et en seconde ?" #Affiche string
+
+puts 10 * 5 * 11 * 60 * 60 #Affiche le résultat du calcul car puts détecte qu'on travail sur des entiers
+
+puts "Est ce que c'est vrai que 3 + 2 < 5 - 7 ?" #Affiche string
+
+puts 3 + 2 < 5 - 7 #Affiche le résultat du calcul car puts détecte qu'on travail sur des entiers
+
+puts "Ca fait combien 3 + 2 ? #{3 + 2}" #Affiche string et calcul de l'intérieur de l'expression entouré par dièse 
+puts "Ca fait combien 5 - 7 ? #{5 - 7}" #Affiche string et calcul de l'intérieur de l'expression entouré par dièse 
+
+puts "Ok, c'est faux alors !" #Affiche string
+
+puts "C'est drole ca faisons en plus :" #Affiche string
+
+puts "Est ce que 5 est plus grand que -2 ? #{5 > -2}" #Affiche string et évalue la valeur du booléen à l'intérieur de dièse et accolade
+puts "Est ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}" #Affiche string et évalue la valeur du booléen à l'intérieur de dièse et accolade
+puts "Est ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}" #Affiche string et évalue la valeur du booléen à l'intérieur de dièse et accolade
